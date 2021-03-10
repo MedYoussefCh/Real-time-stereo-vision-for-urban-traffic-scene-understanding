a8801331c9765f46e2429a0457d22c3ade0a073c
Mohamed Youssef CHOUHAIDI <57833483+MedYoussefCh@users.noreply.github.com>
2021-03-10 18:16:48 +0000

    Add files via upload

---
+++ yoloModule.py
@@ -0,0 +1,108 @@
+# Redmon, J., & Farhadi, A. (2018). Yolov3: An incremental improvement. arXiv preprint arXiv:1804.02767.
+# https://pjreddie.com/media/files/papers/YOLOv3.pdf
+
+
+# https://www.learnopencv.com/deep-learning-based-object-detection-using-yolov3-with-opencv-python-c/
+# https://github.com/spmallick/learnopencv/blob/master/ObjectDetection-YOLO/object_detection_yolo.py
+# https://github.com/spmallick/learnopencv/blob/master/ObjectDetection-YOLO/LICENSE
+
+
+
+# https://pjreddie.com/media/files/yolov3.weights
+# https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg?raw=true
+# https://github.com/pjreddie/darknet/blob/master/data/coco.names?raw=true
+
+################################################################################
+
+import cv2
+import argparse
+import sys
+import math
+import numpy as np
+
+
+def postprocess(image, results, threshold_confidence, threshold_nms):
+    frameHeight = image.shape[0]
+    frameWidth = image.shape[1]
+
+
+
+    classIds = []
+    confidences = []
+    boxes = []
+    for result in results:
+        for detection in result:
+            scores = detection[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > threshold_confidence:
+                center_x = int(detection[0] * frameWidth)
+                center_y = int(detection[1] * frameHeight)
+                width = int(detection[2] * frameWidth)
+                height = int(detection[3] * frameHeight)
+                left = int(center_x - width / 2)
+                top = int(center_y - height / 2)
+                classIds.append(classId)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+
+
+    classIds_nms = []
+    confidences_nms = []
+    boxes_nms = []
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, threshold_confidence, threshold_nms)
+    for i in indices:
+        i = i[0]
+        classIds_nms.append(classIds[i])
+        confidences_nms.append(confidences[i])
+        boxes_nms.append(boxes[i])
+
+
+    return (classIds_nms, confidences_nms, boxes_nms)
+
+
+
+def getOutputsNames(net):
+
+    layersNames = net.getLayerNames()
+    return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# init YOLO CNN object detection model
+
+confThreshold = 0.5  # Confidence threshold
+nmsThreshold = 0.4   # Non-maximum suppression threshold
+inpWidth = 416       # Width of network's input image
+inpHeight = 416      # Height of network's input image
+
+# Load names of classes from file
+
+classes = open("coco.names.txt","r").read().rstrip('\n').split('\n')
+
+# load configuration and weight files for the model and load the network using them
+
+net = cv2.dnn.readNetFromDarknet("yolov3.cfg", "yolov3.weights")
+output_layer_names = getOutputsNames(net)
+
+
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
+
+
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL)
+
+
+def detection(frame):
+
+    tensor = cv2.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
+    net.setInput(tensor)
+    results = net.forward(output_layer_names)
+    classIDs, confidences, boxes = postprocess(frame, results, confThreshold, nmsThreshold)
+
+    # draw resulting detections on image
+    output = []
+    for detected_object in range(0, len(boxes)):
+        box = boxes[detected_object]
+        output.append((classes[classIDs[detected_object]],box))
+    return output
+
+
